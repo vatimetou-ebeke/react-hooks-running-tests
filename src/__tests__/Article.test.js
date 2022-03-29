adc82bdfaf8bea0a3f2807711a985d11b3c8a72a
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-03-29 19:02:52 -0400

    commit tree ba57f10147e3a23a37a7bf1a826b533ae21e7817

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -6,6 +6,9 @@ import Article from "../components/Article";
 
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
+  const element = screen.queryByText("please pass this test");
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  screen.debug(element);
+
+  expect(element).toBeInTheDocument();
 });
